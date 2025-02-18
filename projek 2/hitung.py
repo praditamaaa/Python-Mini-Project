809b1475dcea3e5638da7fba6c890b7f4b3b6976
Praditama Ajmal Hasan <praditamaajmal@gmail.com>
2025-02-18 09:25:48 +0700

    commit pertama

---
+++ projek 2/hitung.py
@@ -0,0 +1,21 @@
+# hitung.py
+
+# funtion menjumlahkan
+def penjumlahan (a, b):
+    return a + b
+
+# function mengurang
+def pengurangan (a, b):
+    return a - b
+
+# function mengalikan
+def perkalian (a, b):
+    return a * b
+
+# function membagi
+def pembagian (a, b):
+    return a / b
+
+# function menampilkan output ke layar
+def display (hasil):
+    print(hasil)
---
+++ projek 2/main.py
@@ -0,0 +1,15 @@
+from hitung import *
+
+# input angka user
+angka1 = float(input("masukkan angka 1: "))
+angka2 = float(input("masukkan angka 2: "))
+
+# memanggil function
+hasil = penjumlahan(angka1, angka2)
+display(hasil)
+hasil = pengurangan(angka1, angka2)
+display(hasil)
+hasil = perkalian(angka1, angka2)
+display(hasil)
+hasil = pembagian(angka1, angka2)
+display(hasil)
